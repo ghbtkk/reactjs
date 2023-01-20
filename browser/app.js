b28e04c92d915de1c97fb54f739d9378ee851d77
ghbtkk <ghbt2611@gmail.com>
2023-01-20 22:33:50 +0900

    230120

--- browser/app.js
+++ browser/app.js
@@ -43,8 +43,10 @@ title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
 
+/*
 window.addEventListener("resize", handleWindowResize);
 window.addEventListener("copy", handleWindowCopy);
 
 window.addEventListener("offline", handleWindowOffline);
 window.addEventListener("online", handleWindowOnline);
+*/
